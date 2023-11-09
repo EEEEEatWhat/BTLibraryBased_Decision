0a560b9e389a2efc3064a5cfb8cb63ec1f536bbf
ynghawU <609381572@qq.com>
2023-11-09 22:43:58 +0800

    modify setGoal

--- src/bt_test/include/bt_test/gain_blood_or_bullet.hpp
+++ src/bt_test/include/bt_test/gain_blood_or_bullet.hpp
@@ -6,59 +6,87 @@
 #include "rclcpp/rclcpp.hpp"
 #include "behaviortree_cpp/action_node.h"
 #include "behaviortree_ros2/bt_action_node.hpp"
-#include "global_interfaces/action/go.hpp"
 #include "behaviortree_ros2/plugins.hpp"
+#include "nav2_msgs/action/navigate_through_poses.hpp"
 
 
 namespace decision_behavior_tree
 {
-    class GainBloodOrBulletAction : public BT::RosActionNode<global_interfaces::action::Go>
+    class GainBloodOrBulletAction : public BT::RosActionNode<nav2_msgs::action::NavigateThroughPoses>
     {   
     public:
         
         GainBloodOrBulletAction(const std::string& name, const BT::NodeConfig& config , const BT::RosNodeParams& params)
-                               : BT::RosActionNode<global_interfaces::action::Go>(name , config, params)
+                               : BT::RosActionNode<nav2_msgs::action::NavigateThroughPoses>(name , config, params)
         {};
 
         static BT::PortsList providedPorts()
         {
-            return { BT::InputPort<double>("goal")}; 
+            // return { BT::InputPort<double>("goal")}; //可以嘎了
+            return {};
         };
-
+        
         bool setGoal(RosActionNode::Goal &goal_) override 
         {
-            auto pos_ = getInput<double>("goal");
-            // goal_.goal = pos_.value();
-            goal_.goal = 1.0;                   // 设置目标 
-                        
-            if(goal_.goal!=0)
+            // 补给区坐标写在config文件里，初始化时加载参数 在这里调取参数然后设置目标值 poses和behavior_tree
+            
+            
+            auto goal_msg = std::make_shared<nav2_msgs::action::NavigateThroughPoses::Goal>();
+             // 创建geometry_msgs::msg::PoseStamped消息的向量
+            std::vector<geometry_msgs::msg::PoseStamped> poses;
+            // 创建一个PoseStamped消息
+            geometry_msgs::msg::PoseStamped pose1;
+            // 设置PoseStamped消息的header字段 坐标系通常是map
+            pose1.header.frame_id = "map";
+            pose1.header.stamp = rclcpp::Clock().now();
+            this->nav_through_poses_goal_ = nav2_msgs::action::NavigateThroughPoses::Goal();
+            // 设置PoseStamped消息的pose字段
+            pose1.pose.position.x = 2.0;
+            pose1.pose.position.y = -0.1;
+            // pose1.pose.position.z = 0.00000226632;
+            // // 设置姿态信息（四元数表示）
+            // pose1.pose.orientation.x = 0.0;
+            // pose1.pose.orientation.y = 0.0;
+            // pose1.pose.orientation.z = 0.0;
+            pose1.pose.orientation.w = 1.0;
+            poses.push_back(pose1); 
+            goal_msg->poses = poses;
+
+
+            goal_.set__poses(poses);
+            return true;
+        }
+
+        BT::NodeStatus onResultReceived(const WrappedResult &wr) override
+        {
+            // wr.result->result 怎么是空消息类型阿
+            if (goal_handle_future.wait_for(std::chrono::seconds(10)) == std::future_status::ready)
             {
-                std::cout << "成功设置目标点坐标----------" << std::endl;
-                return true;
+                // auto goal_handle = goal_handle_future.get()->get_status();
+                if (goal_handle_future.get()->get_status() == rclcpp_action::GoalStatus::STATUS_SUCCEEDED)
+                {
+                    std::cout << "导航完成" << std::endl;
+                    return BT::NodeStatus::SUCCESS;
+                }
+                else
+                {
+                    std::cout << "导航失败" << std::endl;
+                    return BT::NodeStatus::FAILURE;
+                }
             }
             else
             {
-                std::cout << "设置目标点坐标失败----------" << std::endl;
-                return false;
+                std::cout << "无法等待导航完成" << std::endl;
+                return BT::NodeStatus::FAILURE;
             }
-            
-        }
-
-        BT::NodeStatus onResultReceived(const WrappedResult& wr) override
-        {
-        
-        if(wr.result->done == true)
-        std::cout << name().c_str() << ": onResultReceived. Done = true" << std::endl;
-        else
-        std::cout << name().c_str() << ": onResultReceived. Done = false" << std::endl;
-
-        return wr.result->done ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
         }
 
         BT::NodeStatus onFeedback(const std::shared_ptr<const Feedback> feedback) override
         {
-            double now_pos = feedback->current_pos;
-            std::cout << "now_pos:" << now_pos << std::endl ;
+            auto now_pos = feedback->current_pose;
+            std::cout << "now_pos_x:" << now_pos.pose.position.x << std::endl ;
+            std::cout << "now_pos_y:" << now_pos.pose.position.y << std::endl ;
+            std::cout << "now_ori:w" << now_pos.pose.orientation.w << std::endl ;
             return BT::NodeStatus::RUNNING;
         }
 
@@ -69,7 +97,16 @@ namespace decision_behavior_tree
         }
 
     // CreateRosNodePlugin(GainBloodOrBulletAction, "go_blood_or_bullet");// 还得配合插件 
+    private:
+        rclcpp_action::Client<nav2_msgs::action::NavigateThroughPoses>::SharedPtr nav_through_poses_action_client_;
+        nav2_msgs::action::NavigateThroughPoses::Goal nav_through_poses_goal_;
+        rclcpp::Subscription<nav2_msgs::action::NavigateThroughPoses::Impl::GoalStatusMessage>::SharedPtr
+            nav_through_poses_goal_status_sub_;
+        std::shared_future<rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateThroughPoses>::SharedPtr> goal_handle_future;
+
     };
+
+ 
 }  // namespace decision_behavior_tree
 
 
