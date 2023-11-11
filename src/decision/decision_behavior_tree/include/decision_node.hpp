2a55d9f80f64018e4e9f5bb3dba3f4a31182ad29
ynghawU <609381572@qq.com>
2023-11-12 00:07:18 +0800

    add decision_node

--- src/decision/decision_behavior_tree/include/decision_node.hpp
+++ src/decision/decision_behavior_tree/include/decision_node.hpp
@@ -9,15 +9,17 @@
 #include "behaviortree_cpp/behavior_tree.h"
 #include "behaviortree_cpp/bt_factory.h"
 #include "behaviortree_cpp/xml_parsing.h"
-
+#include "yaml-cpp/yaml.h"
 namespace decision_behavior_tree
 {
     class DecisionNode : public rclcpp::Node
     {
     private:
-        /* data */
+        BT::Blackboard::Ptr blackboard_;
+
     public:
         DecisionNode(/* args */);
+        void init();
         ~DecisionNode();
     };
     
